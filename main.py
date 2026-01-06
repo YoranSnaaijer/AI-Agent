e1140d4f0adb81f3b783e06ee5d13fa61a1e4f09
YoranSnaaijer <yoran@snaaijer.nl>
2026-01-06 22:21:15 +0100

    Set up initial test, hardcoded prompt, output (including metadata) output to console

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+# Load environment variables from .env file
+load_dotenv()
+# Set up API key from environment variable
+api_key = os.environ.get("GEMINI_API_KEY")
+# Raise an error if the API key is not found
+if api_key is None:
+    raise RuntimeError("GEMINI_API_KEY not found in environment variables")
+
+
+
 def main():
     print("Hello from ai-agent!")
-
+    # Initialize the GenAI client
+    client = genai.Client(api_key=api_key)
+    prompt ="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
+    if response.usage_metadata is None:
+        raise RuntimeError("Usage metadata is None")
+    print(f"User Prompt: {prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count} \nResponse tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response: \n{response.text}")
 
 if __name__ == "__main__":
     main()
