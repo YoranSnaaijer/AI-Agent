4a77732cd3ddc807222e4fe70dac55e2b075bede
YoranSnaaijer <yoran@snaaijer.nl>
2026-01-07 14:59:14 +0100

    Implemented config file. Implimented get_file_content to read file content with character limit. Updated .gitignore to exclude tests.

---
+++ functions/get_file_content.py
@@ -0,0 +1,20 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    # Ensure the target file is within the working directory
+    working_directory_abs = os.path.abspath(working_directory)
+    target_directory = os.path.normpath(os.path.join(working_directory_abs, file_path))
+    if os.path.commonpath([working_directory_abs, target_directory]) != working_directory_abs:
+        return f"Error: Cannot fetch content from {file_path} as it is outside the permitted working directory"
+    if not os.path.isfile(target_directory):
+        return f"Error: File not found or is not a regular file: {file_path}"
+    try:
+        with open(target_directory, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            # After reading the first MAX_CHARS...
+            if f.read(1):
+                file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        return file_content_string
+    except Exception as e:
+        return f"Error reading file {file_path}: {e}"
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,14 +1,16 @@
 import os
 
 def get_files_info(working_directory, directory="."):
+    # Ensure the target directory is within the working directory
     working_directory_abs = os.path.abspath(working_directory)
     target_directory = os.path.normpath(os.path.join(working_directory_abs, directory))
     if os.path.commonpath([working_directory_abs, target_directory]) != working_directory_abs:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        return f"Error: Cannot list {directory} as it is outside the permitted working directory"
     if not os.path.isdir(target_directory):
-        return f'Error: "{directory}" is not a directory'
+        return f"Error: {directory} is not a directory"
     
     contents_of_directory = []
+    # List files and their sizes
     try:
         for file in os.listdir(target_directory):
             contents_of_directory.append(f"- {file}: file_size={os.path.getsize(os.path.join(target_directory, file))} bytes, is_dir={os.path.isdir(os.path.join(target_directory, file))}")
